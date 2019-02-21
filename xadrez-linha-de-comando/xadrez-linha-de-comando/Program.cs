be4783cc14414512b297d99bb7b4c8d367c51d58
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-02-21 00:02:11 -0300

    Classe Posicao criada

---
+++ xadrez-linha-de-comando/xadrez-linha-de-comando/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tabuleiro;
+
+namespace xadrez_linha_de_comando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+        }
+    }
+}
