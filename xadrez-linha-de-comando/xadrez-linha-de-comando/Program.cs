04f7e223945fd3fa99342ce6e2bb0194ecd5ca81
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-10-19 22:50:46 -0300

    Implementação das classes Rei e Torre, e lógica de Colocar Peça

--- xadrez-linha-de-comando/xadrez-linha-de-comando/Program.cs
+++ xadrez-linha-de-comando/xadrez-linha-de-comando/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_linha_de_comando
 {
@@ -8,6 +9,12 @@ namespace xadrez_linha_de_comando
         static void Main(string[] args)
         {
             Tabuleiro Tabuleiro = new Tabuleiro(8, 8);
+
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Amarela), new Posicao(0, 0));
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Amarela), new Posicao(1, 3));
+            Tabuleiro.ColocarPeca(new Rei(Tabuleiro, Cor.Amarela), new Posicao(2, 4));
+
+            Tela.ImprimirTabuleiro(Tabuleiro);
         }
     }
 }
