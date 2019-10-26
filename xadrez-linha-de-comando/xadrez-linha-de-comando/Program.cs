4a3b017da261e0a6abb73be203bab84bbe21c7ef
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-10-26 11:53:24 -0300

    Criada lógica de cores das peças de xadrez

--- xadrez-linha-de-comando/xadrez-linha-de-comando/Program.cs
+++ xadrez-linha-de-comando/xadrez-linha-de-comando/Program.cs
@@ -8,25 +8,25 @@ namespace xadrez_linha_de_comando
     {
         static void Main(string[] args)
         {
-            //try
-            //{
-            //    Tabuleiro Tabuleiro = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro Tabuleiro = new Tabuleiro(8, 8);
 
-            //    Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Amarela), new Posicao(0, 0));
-            //    Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Amarela), new Posicao(1, 3));
-            //    Tabuleiro.ColocarPeca(new Rei(Tabuleiro, Cor.Amarela), new Posicao(2, 4));
+                Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Amarela), new Posicao(0, 0));
+                Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Branca), new Posicao(1, 3));
+                Tabuleiro.ColocarPeca(new Rei(Tabuleiro, Cor.Amarela), new Posicao(2, 4));
 
-            //    Tela.ImprimirTabuleiro(Tabuleiro);
-            //}
-            //catch (TabuleiroException ex)
-            //{
-            //    Console.WriteLine(ex.Message);
-            //}
+                Tela.ImprimirTabuleiro(Tabuleiro);
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-            PosicaoXadrez posicao = new PosicaoXadrez('C', 7);
+            //PosicaoXadrez posicao = new PosicaoXadrez('C', 7);
 
-            Console.WriteLine(posicao);
-            Console.WriteLine(posicao.ToPosicao());
+            //Console.WriteLine(posicao);
+            //Console.WriteLine(posicao.ToPosicao());
         }
     }
 }
--- xadrez-linha-de-comando/xadrez-linha-de-comando/Tela.cs
+++ xadrez-linha-de-comando/xadrez-linha-de-comando/Tela.cs
@@ -9,14 +9,34 @@ namespace xadrez_linha_de_comando
         {
             for (int i = 0; i < tabuleiro.Linhas; i++)
             {
+                Console.Write(8-i+" ");
                 for (int j = 0; j < tabuleiro.Colunas; j++)
                 {
-                    if(tabuleiro.Peca(i, j) != null)
-                        Console.Write(tabuleiro.Peca(i, j) + " ");
+                    if (tabuleiro.Peca(i, j) != null)
+                    {
+                        //Console.Write(tabuleiro.Peca(i, j) + " ");
+                        ImprimirPeca(tabuleiro.Peca(i, j));
+                        Console.Write(" ");
+                    }
                     else
                         Console.Write("- ");
                 }
                 Console.WriteLine();
+            }
+
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if(peca.Cor == Cor.Branca)
+                Console.Write(peca);
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
