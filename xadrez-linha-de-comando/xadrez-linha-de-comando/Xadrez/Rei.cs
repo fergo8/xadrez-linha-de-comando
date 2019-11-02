265d24fed98b37d011bc3e8e135d1cf592271b90
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-11-02 18:54:21 -0300

    Movimentação das peças Rei e Torre implementadas

--- xadrez-linha-de-comando/xadrez-linha-de-comando/Xadrez/Rei.cs
+++ xadrez-linha-de-comando/xadrez-linha-de-comando/Xadrez/Rei.cs
@@ -13,5 +13,66 @@ namespace xadrez
         {
             return "R";
         }
+
+        private bool PodeMover(Posicao posicao)
+        {
+            Peca Peca = Tabuleiro.Peca(posicao);
+            return Peca == null || Peca.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPosiveis()
+        {
+            bool[,] matriz = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+
+            Posicao Posicao = new Posicao(0, 0);
+
+            // posição norte
+            Posicao.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+
+            if (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+            // posição nordeste
+            Posicao.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+
+            if (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+            // posição leste
+            Posicao.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+
+            if (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+            // posição sudeste
+            Posicao.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+
+            if (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+            // posição sul
+            Posicao.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+
+            if (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+            // posição sudoeste
+            Posicao.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+
+            if (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+            // posição oeste
+            Posicao.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+
+            if (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+            // posição noroeste
+            Posicao.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+
+            if (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+        }
     }
 }
--- xadrez-linha-de-comando/xadrez-linha-de-comando/Xadrez/Torre.cs
+++ xadrez-linha-de-comando/xadrez-linha-de-comando/Xadrez/Torre.cs
@@ -13,5 +13,62 @@ namespace xadrez
         {
             return "T";
         }
+
+        private bool PodeMover(Posicao posicao)
+        {
+            Peca Peca = Tabuleiro.Peca(posicao);
+            return Peca == null || Peca.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPosiveis()
+        {
+            bool[,] matriz = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+
+            Posicao Posicao = new Posicao(0, 0);
+
+            // posição norte
+            Posicao.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+            {
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+                if(Tabuleiro.Peca(Posicao) != null && Tabuleiro.Peca(Posicao).Cor != Cor) break;
+                
+                Posicao.Linha = Posicao.Linha - 1;
+            }
+
+            // posição leste
+            Posicao.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+            {
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+                if (Tabuleiro.Peca(Posicao) != null && Tabuleiro.Peca(Posicao).Cor != Cor) break;
+
+                Posicao.Coluna = Posicao.Coluna + 1;
+            }
+
+            // posição sul
+            Posicao.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            while (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+            {
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+                if (Tabuleiro.Peca(Posicao) != null && Tabuleiro.Peca(Posicao).Cor != Cor) break;
+
+                Posicao.Linha = Posicao.Linha + 1;
+            }
+
+            // posição oeste
+            Posicao.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            while (Tabuleiro.PosicaoValida(Posicao) && PodeMover(Posicao))
+            {
+                matriz[Posicao.Linha, Posicao.Coluna] = true;
+
+                if (Tabuleiro.Peca(Posicao) != null && Tabuleiro.Peca(Posicao).Cor != Cor) break;
+
+                Posicao.Coluna = Posicao.Coluna - 1;
+            }
+        }
     }
 }
